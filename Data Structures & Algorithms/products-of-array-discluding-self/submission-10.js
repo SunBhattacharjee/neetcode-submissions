67983c9a1d382b6d4d1a0b58fabbc1a7e9d3cf31
Sun Bhattacharjee <69688596+SunBhattacharjee@users.noreply.github.com>
2026-07-14 09:34:39 +0530

    Add: products-of-array-discluding-self - submission-10

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-10.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const n = nums.length;
+        let prefix = 1;
+        let res = Array(n).fill(1);
+        for(let i = 0; i < n; i++) {
+            res[i] = prefix;
+            prefix = prefix * nums[i];
+        }
+        prefix = 1;
+        for(let i = n - 1; i >= 0; i--) {
+            res[i] = res[i] * prefix;
+            prefix = prefix * nums[i];
+        }
+
+        return res;
+    }
+}
