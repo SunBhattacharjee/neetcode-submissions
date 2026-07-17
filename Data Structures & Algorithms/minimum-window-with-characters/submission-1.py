8563e948829cc55a4679216d4e36b57203c176c7
Sun Bhattacharjee <69688596+SunBhattacharjee@users.noreply.github.com>
2026-07-17 13:32:38 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if s == "":
+            return ""
+
+        tcount, window = {}, {}
+        for c in t:
+            tcount[c] = 1 + tcount.get(c, 0)
+
+        found, need = 0, len(tcount)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+            if c in tcount and window[c] == tcount[c]:
+                found += 1
+            
+            while found == need:
+                res = [l, r]
+                resLen = min(resLen, r - l + 1)
+                window[s[l]] -= 1
+                if s[l] in tcount and window[s[l]] < tcount[s[l]]:
+                    found -= 1
+                l += 1
+
+        if resLen == float("infinity"):
+            return ""
+        else:
+            return s[res[0]:res[1] + 1]
